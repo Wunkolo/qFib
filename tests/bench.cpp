8e8641853c4cd40a03bd654b4912d08748e69493
Wunkolo <Wunkolo@gmail.com>
2018-11-23 20:46:11 -0800

    Add AVX512 variation of Chun-Min Chang

--- tests/bench.cpp
+++ tests/bench.cpp
@@ -146,20 +146,20 @@ struct ChunMin : FibMethod
 		// the highest bit of n(mask = 2^(h-1) at first), and it will be shifted right
 		// iteratively to do `AND` operation with `n` to check `n_j` is odd or even,
 		// where n_j is defined below.
-		for( std::uint64_t mask = 1 << (h - 1); mask; mask >>= 1)
+		for( std::uint64_t mask = 1 << (h - 1); mask; mask >>= 1 )
 		{ // Run h times!
 		// Let j = h-i (looping from i = 1 to i = h), n_j = floor(n / 2^j) = n >> j
 		// (n_j = n when j = 0), k = floor(n_j / 2), then a = F(k), b = F(k+1) now.
 			std::uint64_t c = a * (2 * b - a); // F(2k) = F(k) * [ 2 * F(k+1) – F(k) ]
 			std::uint64_t d = a * a + b * b;   // F(2k+1) = F(k)^2 + F(k+1)^2
 
-			if (mask & n)
+			if( mask & n )
 			{ // n_j is odd: k = (n_j-1)/2 => n_j = 2k + 1
 				a = d;        //   F(n_j) = F(2k + 1)
 				b = c + d;    //   F(n_j + 1) = F(2k + 2) = F(2k) + F(2k + 1)
 			}
 			else
-			{        // n_j is even: k = n_j/2 => n_j = 2k
+			{ // n_j is even: k = n_j/2 => n_j = 2k
 				a = c;        //   F(n_j) = F(2k)
 				b = d;        //   F(n_j + 1) = F(2k + 1)
 			}
@@ -168,6 +168,68 @@ struct ChunMin : FibMethod
 		return a;
 	}
 };
+
+#if defined(__AVX512F__) || defined(_MSC_VER)
+struct ChunMinAVX512 : FibMethod
+{
+	const char* GetName() const override
+	{
+		return "Chun-Min - AVX512";
+	}
+
+	std::uint64_t operator()(std::uint64_t n) override
+	{
+		#ifdef _MSC_VER
+		const std::uint64_t h = 64 - __lzcnt64(n);
+		#else
+		const std::uint64_t h = 64 - __builtin_clzll(n);
+		#endif
+
+		__m128i ab = _mm_set_epi64x(1,0);
+		for( std::uint64_t mask = 1 << (h - 1); mask; mask >>= 1 )
+		{
+			const __m128i ab_sq = _mm_mullo_epi64( ab, ab );
+			const __m128i cd = _mm_add_epi64(
+				_mm_mullo_epi64(
+					_mm_mullo_epi64(
+						ab,
+						_mm_shuffle_epi32(ab,0b11'10'11'10)
+					),
+					_mm_set_epi64x( 1, 2 )
+				),
+				_mm_mullo_epi64(
+					_mm_broadcastq_epi64(ab_sq),
+					_mm_set_epi64x(1,-1)
+				)
+			);
+			const __m128i cd_sum = _mm_add_epi64(
+				cd,
+				_mm_alignr_epi64(
+					cd,
+					cd,
+					1
+				)
+			);
+			if( mask & n )
+			{
+				ab = _mm_permutex2var_epi64(
+					cd,
+					_mm_set_epi64x(
+						2,1
+					),
+					cd_sum
+				);
+			}
+			else
+			{
+				ab = cd;
+			}
+		}
+
+		return _mm_extract_epi64(ab,0);
+	}
+};
+#endif
 }
 
 const static std::unique_ptr<FibMethod> FibMethods[] = {
@@ -176,6 +238,9 @@ const static std::unique_ptr<FibMethod> FibMethods[] = {
 	std::make_unique<Methods::Stack2Reg>(),
 	std::make_unique<Methods::MatrixExp>(),
 	std::make_unique<Methods::ChunMin>(),
+#if defined(__AVX512F__) || defined(_MSC_VER)
+	std::make_unique<Methods::ChunMinAVX512>(),
+#endif
 };
 
 
